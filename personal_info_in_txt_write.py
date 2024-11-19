cdecb1aee3eeeaebcabebe8f1deb1d390668a679
@Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-11-19 10:50:15 +0800

    This is the pseudocode or the flow of my initial code, I want to utilize loop and dictionary for ease and to update the dictionary and continuously adding new name and its values.

---
+++ personal_info_in_txt_write.py
@@ -0,0 +1,7 @@
+#use the loop
+#ask the user to input the name
+#insert the name into a dictionary
+#ask the user to input their personal information
+#insert the personal information into the name's dictionary
+#enter the dictionary for writing in the text file
+#loop
