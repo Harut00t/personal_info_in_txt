f3cc870cba2919bda8738577ab72f438392cb2cc
@Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-11-28 15:53:24 +0800

    I made the reading processes as a line by line for readability. It is also the looping, checking the full name and reading the .txt file by 6 lines.

--- personal_info_in_txt_read.py
+++ personal_info_in_txt_read.py
@@ -7,3 +7,25 @@ middlename = input("please enter your middle name: ").strip()
 name = f"{lastname}, {firstname}, {middlename}"
 
 #Read the .txt file with the name
+with open(".\personal_info.txt", "r") as file_handle:
+    found = False  #Flag to check if the name is found
+    for line in file_handle: #Loop
+        if name in line: #Case insensitive checking
+            found = True
+            line1 = file_handle.readline().strip()
+            line2 = file_handle.readline().strip()
+            line3 = file_handle.readline().strip()
+            line4 = file_handle.readline().strip()
+            line5 = file_handle.readline().strip()
+            line6 = file_handle.readline().strip()
+
+            print(f"Name: {name}")
+            print(f"{line1}")
+            print(f"{line2}")
+            print(f"{line3}")
+            print(f"{line4}")
+            print(f"{line5}")
+            print(f"{line6}")
+    
+    if not found:
+        print(f"Name '{name}' not found in the file.")
