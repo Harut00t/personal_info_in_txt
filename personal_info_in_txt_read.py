d83394610002d3479eccc56a9f452d2b11b5e2af
@Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-11-28 15:38:26 +0800

    I made a format of the name to a full name for readability and organized manner.

--- personal_info_in_txt_read.py
+++ personal_info_in_txt_read.py
@@ -1,4 +1,9 @@
 #Input the name
-name = (input("Please put your nickname here: "))
+firstname = input("please enter your first name: ").strip()
+lastname = input("please enter your last name: ").strip()
+middlename = input("please enter your middle name: ").strip()
+
+#Format
+name = f"{lastname}, {firstname}, {middlename}"
 
 #Read the .txt file with the name
